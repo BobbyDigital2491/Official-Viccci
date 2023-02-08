00800d94cb84dd17a9006f46a2176da0f017d2ad
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-08 18:48:56 -0500

    Update Promo.tsx

--- components/Promo.tsx
+++ components/Promo.tsx
@@ -3,10 +3,7 @@ import React from 'react'
 const Promo = () => {
   return (
     <div>
-      <video className="w-full"  muted controls>
-        <source src="https://twitter.com/i/status/1623461456309022724" type="video/mp4"/>
-        Your browser does not support the video tag.
-      </video>
+      <iframe width="560" height="515" src="https://www.youtube.com/embed/NfC9duIA4wY" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className='w-full  max-w-full'></iframe>
     </div>
   )
 }
