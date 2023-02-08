d2e21f71e106955438931be273a0e7607411d01e
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-08 14:04:24 -0500

    Update Promo.tsx

--- components/Promo.tsx
+++ components/Promo.tsx
@@ -4,7 +4,7 @@ const Promo = () => {
   return (
     <div>
       <video className="w-full"  muted controls>
-  <source src="vic.mp4" type="video/mp4"/>
+  <source src="https://youtu.be/t8U7GRrlYW8?t=160" type="video/mp4"/>
   Your browser does not support the video tag.
 </video>
     </div>
