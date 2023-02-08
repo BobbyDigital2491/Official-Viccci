2f552d6f125468e54ebe72a54de761e08b59189d
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-08 18:08:37 -0500

    Update Promo.tsx

--- components/Promo.tsx
+++ components/Promo.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Promo = () => {
   return (
     <div>
+      <iframe width="678" height="385" src="https://www.youtube.com/embed/NfC9duIA4wY" title="Introducing Viccci Protocol!!💎" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <video className="w-full"  muted controls>
   <source src="https://youtu.be/t8U7GRrlYW8?t=160" type="video/mp4"/>
   Your browser does not support the video tag.
