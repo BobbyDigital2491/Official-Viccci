bf9539cd0f4125af266923aede97a0bc80bc0a67
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-08 19:39:02 -0500

    Update Hero3.tsx

--- components/Hero3.tsx
+++ components/Hero3.tsx
@@ -19,7 +19,7 @@ const Hero3 = () => {
         Each Co - Founder of Viccci Protocol brings a diverse range of skill sets to the decentralized social exchange. 
       The team is comprised of individuals with expertise in the blockchain technology field, software development, finance, 
       marketing and data analytics. The team brings a passion for creating a secure and accessible platform for the exchange. 
-      This is the motivating force behind their commitment to scaling $VCI. This combination of expertise and business savvy 
+      This is the motivating force behind their commitment to scaling $VCP. This combination of expertise and business savvy 
       enabled them to build a strong foundation for the platform and to ensures continued growth and success. 
         </p>
         <div>
