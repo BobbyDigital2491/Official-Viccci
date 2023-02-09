1f3e3a85ba8fcb138ccfa94df90e8f14539ffd87
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-08 19:11:58 -0500

    Update ExtendedT.tsx

--- components/ExtendedT.tsx
+++ components/ExtendedT.tsx
@@ -53,7 +53,7 @@ const ExtendedT = () => {
                       <a href="#" className='text-white'>Quentyn <span className='blue'>Sellers</span></a>
                   </h3>
                   <span className="text-white dark:text-gray-400">Research and Development</span>
-                  <p className="mt-3 mb-4 font-light text-white dark:text-white">Quentyn drives the technical strategy of the Viccci platform and brand.</p>
+                  <p className="mt-3 mb-4 font-light text-white dark:text-white">Quentyn drives the research & development strategy of the Viccci platform.</p>
                   <ul className="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" className="text-blue-500 hover:text-gray-900 dark:hover:text-white">
@@ -87,7 +87,7 @@ const ExtendedT = () => {
                       <a href="#" className='text-white'>Tawny <span className='blue'>Jones</span></a>
                   </h3>
                   <span className="text-white dark:text-gray-400">Market Analyst / Research & Development</span>
-                  <p className="mt-3 mb-4 font-light text-whit dark:text-white">TJ drives the technical strategy of the Viccci platform and brand.</p>
+                  <p className="mt-3 mb-4 font-light text-whit dark:text-white">TJ drives the social media as well as research & development of Viccci.</p>
                   <ul className="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" className="text-blue-500 hover:text-gray-900 dark:hover:text-white">
@@ -121,7 +121,7 @@ const ExtendedT = () => {
                       <a href="#" className='text-white'>Quan <span className='blue'>Adams</span></a>
                   </h3>
                   <span className="text-white dark:text-gray-400">Lead Artist</span>
-                  <p className="mt-3 mb-4 font-light text-white dark:text-white">Quan drives the technical strategy of the Viccci platform and brand.</p>
+                  <p className="mt-3 mb-4 font-light text-white dark:text-white">Quan is the lead artist of the Viccci platform and brand.</p>
                   <ul className="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" className="text-blue-500 hover:text-gray-900 dark:hover:text-white">
