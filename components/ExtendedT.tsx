2b1494cfedbd82070309bcd768705d0ab2c61978
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-09 20:13:40 -0500

    Update ExtendedT.tsx

--- components/ExtendedT.tsx
+++ components/ExtendedT.tsx
@@ -69,7 +69,7 @@ const ExtendedT = () => {
                       <a href="#" className='text-white'>Tawny <span className='blue'>Jones</span></a>
                   </h3>
                   <span className="text-white dark:text-gray-400">Market Analyst / Research & Development</span>
-                  <p className="mt-3 mb-4 font-light text-whit dark:text-white">TJ drives the technical strategy of the Viccci platform and brand.</p>
+                  <p className="mt-3 mb-4 font-light text-whit dark:text-white">Tawny contributes unique social media strategies and creative concepts for the Viccci platform and brand.</p>
                   <ul className="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" className="text-blue-500 hover:text-gray-900 dark:hover:text-white">
