01634d0f7b95a198edc8831ae95985adb8aa07ae
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-15 22:03:19 -0500

    Update ExtendedT.tsx

--- components/ExtendedT.tsx
+++ components/ExtendedT.tsx
@@ -94,7 +94,7 @@ const ExtendedT = () => {
                       <a href="#" className='text-white'>Quan <span className='blue'>Adams</span></a>
                   </h3>
                   <span className="text-white dark:text-gray-400">Lead Artist</span>
-                  <p className="mt-3 mb-4 font-light text-white dark:text-white">Quan drives the technical strategy of the Viccci platform and brand.</p>
+                  <p className="mt-3 mb-4 font-light text-white dark:text-white">Quan is the lead artist fro for Viccci Protocol.</p>
                   <ul className="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" className="text-blue-500 hover:text-gray-900 dark:hover:text-white">
