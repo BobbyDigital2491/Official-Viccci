4997a6036f856cc480fb8e52b43d69a000f77e27
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-23 13:03:08 -0500

    Update Seo.tsx

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,58 +1,49 @@
 import Head from "next/head";
+
 interface Props {
   title: string;
   description: string;
   image: string;
   url: string;
 }
-export default function SEO({ title, description, image,url }: Props) {
+
+export default function SEO({ title, description, image, url }: Props) {
   return (
     <Head>
+      {/* Primary Meta Tags */}
       <title>{title}</title>
+      <meta name="title" content={title} />
       <meta name="description" content={description} />
-      <meta
-        name="viewport"
-        content="width=device-width,minimum-scale=1,initial-scale=1"
-      />
-      <meta property="og:type" content="website" />
-      <meta name="og:title" property="og:title" content={title} />
-      <meta
-        name="og:description"
-        property="og:description"
-        content={description}
-      />
-      <meta property="og:site_name" content="Viccci.xyz" />
 
-      <meta name="twitter:card" content="Viccci.xyz" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:site" content="@propernoun" />
-      <meta name="twitter:creator" content="@propernounco" />
-
-    
-        <meta property="og:image" content={`${image}`} />
-     <meta name="twitter:image" content={`${image}`} />
-
-              {/* Open Graph / Facebook */}
+      {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
+      <meta property="og:image:alt" content={title} />
       <meta property="og:site_name" content={title} />
+
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={image} />
-      <meta name="robots" content="Index" />
-      {/*Favicon*/}
-      <link rel="apple-touch-icon" sizes="120x120" href="./log.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="./log.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="./log.png" />
 
-
+      {/* Other Meta Tags */}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+      <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+      
+      {/* Favicon */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/log.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/log.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/log.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#00aba9" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 }
