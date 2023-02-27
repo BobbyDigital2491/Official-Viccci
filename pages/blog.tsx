01ca94529b8d669f36502943e6aba59261d198c3
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-02-27 18:24:58 -0500

    Update blog.tsx

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -9,8 +9,8 @@ const blog = () => {
     
     <div>  
       <NavComponent />
-      <SEO title={"Viccci Protocol"} 
-      description={"Viccci Protocol is a decentralized social exchange built on the cutting-edge Polygon Layer 2 technology. What sets it apart from other decentralized exchanges is its focus on creating a community-driven platform that prioritizes the needs of its users. Through its unique governance model, users have a direct say in the development and direction of the platform, and can earn rewards for participating in the ecosystem. Additionally, the use of Polygon's high-performance Layer 2 technology allows for fast, low-cost transactions, making it a suitable choice for both casual and professional traders. Overall, Viccci Protocol's combination of community empowerment and technical innovation makes it a standout player in the decentralized exchange space."} 
+      <SEO title={"Viccci Protocol Blog"} 
+      description={"Blog Under Construction."} 
       image={"log.png"} 
       url={"Viccci.xyz"}  />
       
